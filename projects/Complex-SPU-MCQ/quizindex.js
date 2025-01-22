5740256b71f847c90913a282bdf5175bbe0fb57d
jaygmehta <135193378+jaygmehta@users.noreply.github.com>
2025-01-22 14:00:23 +0000

    Deploying to gh-pages from @ jaygmehta/jaygmehta.github.io@ea7b48c14cfdc702eeb6a8f1b81c4229a7b8575f 🚀

---
+++ projects/Complex-SPU-MCQ/quizindex.js
@@ -0,0 +1,8 @@
+var QuizTitles = [
+  ['Quiz 1. Numbers and sets', 'complex2-spu-mcq-1.html'],
+  ['Quiz 2. Polar form and roots of complex numbers', 'complex2-spu-mcq-2.html']
+];
+// construct the drop down menu if QuizTitles has some entries
+if (QuizTitles.length > 0 && quizindex_menu) {
+    create_quizindex_menu();
+}
