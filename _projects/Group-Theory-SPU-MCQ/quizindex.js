342863180b033d8953bf8b8ca2d2eeedf60f4bf7
jaygmehta <jaygmehta@gmail.com>
2025-07-09 23:59:49 +0530

    group theory mcq sem 3

---
+++ _projects/Group-Theory-SPU-MCQ/quizindex.js
@@ -0,0 +1,19 @@
+var QuizTitles = [
+  ['Quiz 1. PS03EMTH36-(13-04-2022)', 'PS03EMTH36-(13-04-2022).html'],
+  ['Quiz 2. PS03EMTH36-(25-10-2021)', 'PS03EMTH36-(25-10-2021).html'],
+  ['Quiz 3. PS03EMTH36-(01-01-2021)', 'PS03EMTH36-(01-01-2021).html'],
+  ['Quiz 4. PS03EMTH08-(01-01-2021)', 'PS03EMTH08-(01-01-2021).html'],
+  ['Quiz 5. PS03EMTH36-(25-11-2019)', 'PS03EMTH36-(25-11-2019).html'],
+  ['Quiz 6. PS03EMTH36-(01-04-2019)', 'PS03EMTH36-(01-04-2019).html'],
+  ['Quiz 7. PS03EMTH08-(01-04-2019)', 'PS03EMTH08-(01-04-2019).html'],
+  ['Quiz 8. PS04EMTH37-(30-03-2019)', 'PS04EMTH37-(30-03-2019).html'],
+  ['Quiz 9. PS03EMTH36-(03-11-2018)', 'PS03EMTH36-(03-11-2018).html'],
+  ['Quiz 10. PS03EMTH08-(03-11-2018)', 'PS03EMTH08-(03-11-2018).html'],
+  ['Quiz 11. PS03EMTH08-(21-04-2018)', 'PS03EMTH08-(21-04-2018).html'],
+  ['Quiz 12. PS03EMTH08-(11-11-2017)', 'PS03EMTH08-(11-11-2017).html'],
+  ['Quiz 13. PS03EMTH08-(21-04-2017)', 'PS03EMTH08-(21-04-2017).html']
+];
+// construct the drop down menu if QuizTitles has some entries
+if (QuizTitles.length > 0 && quizindex_menu) {
+    create_quizindex_menu();
+}
