2b617963d5350c17649de7b9e32c6d995fd423b6
jaygmehta <135193378+jaygmehta@users.noreply.github.com>
2025-07-11 08:43:42 +0000

    Deploying to gh-pages from @ jaygmehta/jaygmehta.github.io@837bb30dfb564cba13c1a8365803515906d6b40b 🚀

---
+++ projects/NTC-SPU-MCQ/quizindex.js
@@ -0,0 +1,17 @@
+var QuizTitles = [
+  ['Quiz 1. PS03EMTH55-(11-11-2022)', 'PS03EMTH55-(11-11-2022).html'],
+  ['Quiz 2. PS03EMTH55-(27-04-2023)', 'PS03EMTH55-(27-04-2023).html'],
+  ['Quiz 3. PS04EMTH59-(01-05-2023)', 'PS04EMTH59-(01-05-2023).html'],
+  ['Quiz 4. PS03EMTH55-(01-11-2023)', 'PS03EMTH55-(01-11-2023).html'],
+  ['Quiz 5. PS04EMTH59-(04-11-2023)', 'PS04EMTH59-(04-11-2023).html'],
+  ['Quiz 6. PS03EMTH55-(12-04-2024)', 'PS03EMTH55-(12-04-2024).html'],
+  ['Quiz 7. PS04EMTH59-(09-04-2024)', 'PS04EMTH59-(09-04-2024).html'],
+  ['Quiz 8. PS03EMTH55-(06-12-2024)', 'PS03EMTH55-(06-12-2024).html'],
+  ['Quiz 9. PS04EMTH59-(27-11-2024)', 'PS04EMTH59-(27-11-2024).html'],
+  ['Quiz 10. PS03EMTH55-(16-04-2025)', 'PS03EMTH55-(16-04-2025).html'],
+  ['Quiz 11. PS04EMTH59-(16-04-2025)', 'PS04EMTH59-(16-04-2025).html']
+];
+// construct the drop down menu if QuizTitles has some entries
+if (QuizTitles.length > 0 && quizindex_menu) {
+    create_quizindex_menu();
+}
